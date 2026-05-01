dbbfb2adddda443d8b071eb68cee4c8dd8943a9b
Bassel Raafat <basselr11@gmail.com>
2026-05-02 00:55:45 +0300

    change the base url

--- src/utils/apiInstance.ts
+++ src/utils/apiInstance.ts
@@ -3,8 +3,7 @@ import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
 axios.defaults.withCredentials = true;
 
 export const api = axios.create({
-	baseURL:
-		"https://blog-backend-3x280uojj-basselraafats-projects.vercel.app/api",
+	baseURL: "https://blog-backend-fawn-six.vercel.app/api",
 	timeout: 5000,
 });
 api.interceptors.request.use((config) => {
