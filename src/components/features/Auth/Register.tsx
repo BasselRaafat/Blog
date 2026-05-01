2a97242aa63fc4c8f541259fc9dd1c1cfd9bdbb9
Bassel Raafat <basselr11@gmail.com>
2026-05-02 01:24:23 +0300

    chnaged the base url

--- src/components/features/Auth/Register.tsx
+++ src/components/features/Auth/Register.tsx
@@ -1,47 +1,47 @@
-import { toast } from "react-toastify";
 import type { RegisterRequest } from "../../../types/auth";
 import { toFormData } from "../../../utils/toFormData";
-import { useForm, type SubmitHandler } from "react-hook-form";
 import { useRegister } from "../../../hooks/useRegister";
+import { useState } from "react";
 
 export default function Register() {
-	// const [form, setForm] = useState<RegisterRequest>({
-	// 	email: "",
-	// 	name: "",
-	// 	password: "",
-	// 	profilePic: null,
-	// });
+	const [form, setForm] = useState<RegisterRequest>({
+		email: "",
+		name: "",
+		password: "",
+		profilePic: null,
+	});
 	const registerMutation = useRegister();
 
-	const onSubmit: SubmitHandler<RegisterRequest> = (data) => {
-		const formData = toFormData(data);
+	const onSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const formData = toFormData(form);
 		registerMutation.mutate(formData);
 	};
-	// const handleOnChange = (
-	// 	e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
-	// ) => {
-	// 	const { value, name, type, files } = e.target;
-	// 	const newForm = {
-	// 		...form,
-	// 		[name]: type === "file" ? (files?.[0] ?? null) : value,
-	// 	};
+	const handleOnChange = (
+		e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+	) => {
+		const { value, name, type, files } = e.target;
+		const newForm = {
+			...form,
+			[name]: type === "file" ? (files?.[0] ?? null) : value,
+		};
 
-	// 	setForm(newForm);
-	// };
-	const {
-		register,
-		handleSubmit,
-		// watch,
-		formState: { errors },
-	} = useForm<RegisterRequest>();
-	if (registerMutation.isError)
-		toast.error("something went wrong please try again");
+		setForm(newForm);
+	};
+	// const {
+	// 	register,
+	// 	handleSubmit,
+	// 	// watch,
+	// 	formState: { errors },
+	// } = useForm<RegisterRequest>();
+	// if (registerMutation.isError)
+	// 	toast.error("something went wrong please try again");
 	return (
 		<div>
 			<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs p-4">
 				<legend className="fieldset-legend">Register</legend>
 
-				<form method="post" onSubmit={handleSubmit(onSubmit)}>
+				<form method="post" onSubmit={onSubmit}>
 					<label className="label" htmlFor="name">
 						Name
 					</label>
@@ -50,10 +50,11 @@ export default function Register() {
 						className="input block"
 						id="name"
 						placeholder="Your Name"
-						{...register("name")}
-						// onChange={handleOnChange}
+						// {...register("name")}
+						onChange={handleOnChange}
+						name="name"
 					/>
-					{errors.name && <span>{errors.name.message}</span>}
+					{/* {errors.name && <span>{errors.name.message}</span>} */}
 
 					<label className="label" htmlFor="email">
 						Email
@@ -63,10 +64,11 @@ export default function Register() {
 						className="input block"
 						id="email"
 						placeholder="Email"
-						{...register("email")}
-						// onChange={handleOnChange}
+						// {...register("email")}
+						onChange={handleOnChange}
+						name="email"
 					/>
-					{errors.email && <span>{errors.email.message}</span>}
+					{/* {errors.email && <span>{errors.email.message}</span>} */}
 
 					<label htmlFor="profile-pic" className="label">
 						Profile Picture
@@ -76,10 +78,11 @@ export default function Register() {
 						className="file-input block"
 						id="profile-pic"
 						accept="image/*"
-						{...register("profilePic")}
-						// onChange={handleOnChange}
+						// {...register("profilePic")}
+						onChange={handleOnChange}
+						name="profilePic"
 					/>
-					{errors.profilePic && <span>{errors.profilePic.message}</span>}
+					{/* {errors.profilePic && <span>{errors.profilePic.message}</span>} */}
 					<label className="label" htmlFor="password">
 						Password
 					</label>
@@ -89,11 +92,12 @@ export default function Register() {
 						className="input block"
 						id="password"
 						placeholder="Password"
-						{...register("password")}
-						// onChange={handleOnChange}
+						// {...register("password")}
+						onChange={handleOnChange}
+						name="password"
 					/>
 
-					{errors.password && <span>{errors.password.message}</span>}
+					{/* {errors.password && <span>{errors.password.message}</span>} */}
 					<button
 						className="btn btn-neutral mt-4 block"
 						type="submit"
